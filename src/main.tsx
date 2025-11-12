27c50952c47c63ad3ec009e7461aef9bbfa98ab6
bykbyk0401 <bykbyk0401@gmail.com>
2025-11-12 14:49:22 +0900

    refactor/#4: indes.css 삭제, 폰트 설정 이동

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from './App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './style/theme';
 import { GlobalStyle } from './style/GlobalStyle';
-import './style/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
