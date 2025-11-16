f4247ddf311cbe9fd72d2539601149a08a0b5013
wldy4627 <wldy4627@gmail.com>
2025-11-17 00:39:32 +0900

    feat/#14: 회원가입 단계(1~3) 페이지 연결 및 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path='/signup' element={<SignupPage />} />
       </Routes>
     </>
   );
