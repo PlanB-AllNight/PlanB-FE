cb8cd3910274c93087e2782cc8c41e18cc5a8c27
wldy4627 <wldy4627@gmail.com>
2025-11-17 01:38:56 +0900

    feat/#14: 비밀번호 Input type 추가

--- src/components/signup/Step2UserInfo.tsx
+++ src/components/signup/Step2UserInfo.tsx
@@ -90,6 +90,7 @@ const Step2UserInfo = ({ onPrev, onNext }: Step2Props) => {
                         variant="primary"
                         placeholder="영문, 숫자, 특수문자 포함 8자 이상 20자 이하"
                         name="password"
+                        type="password"
                         value={form.password}
                         onChange={handleChange}
                     />
@@ -99,6 +100,7 @@ const Step2UserInfo = ({ onPrev, onNext }: Step2Props) => {
                         variant="primary"
                         placeholder="비밀번호 확인"
                         name="confirmPassword"
+                        type="password"
                         value={form.confirmPassword}
                         onChange={handleChange}
                     />
