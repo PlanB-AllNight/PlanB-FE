64159f983ec201bbb7a35b5beda91776effde62e
bykbyk0401 <bykbyk0401@gmail.com>
2025-11-21 04:46:59 +0900

    feat/#22: 페이지 이동 시 상단 스크롤 처리, 헤더 상단 고정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import styled from 'styled-components';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import ScrollToTop from './components/ScrollToTop';
+import SimulationPage from './pages/SimulationPage';
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/simulate" element={<SimulationPage />} />
       </Routes>
     </>
   );
