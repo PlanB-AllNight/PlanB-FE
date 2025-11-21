be0bc4c5ac236309d161a2810ce9203805c8bd14
bykbyk0401 <bykbyk0401@gmail.com>
2025-11-21 11:25:18 +0900

    chore/#22: rebase 충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
-import ScrollToTop from './components/ScrollToTop';
-import SimulationPage from './pages/SimulationPage';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
 import ScrollToTop from './components/ScrollToTop';
