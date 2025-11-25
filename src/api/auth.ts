616e2d5ccafd62f58294812f12ce2835c5468a50
wldy4627 <wldy4627@gmail.com>
2025-11-26 02:07:23 +0900

    feat/#39: 로그인 API 연결

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -10,3 +10,13 @@ export const registerUser = async (formData: {
     const res = await api.post("/users/register", formData);
     return res.data;
 };
+
+export interface LoginForm {
+    userId: string;
+    password: string;
+}
+
+export const loginUser = async (data: LoginForm) => {
+    const res = await api.post("/users/login", data);
+    return res.data;
+}
