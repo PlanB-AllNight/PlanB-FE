02466967165a7fbd9c562d56e667148906acea09
bykbyk0401 <bykbyk0401@gmail.com>
2025-11-29 20:46:39 +0900

    feat/#36: 지원정보 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import SignupPage from './pages/SignupPage';
-import LoginPage from './pages/LoginPage';
-import ScrollToTop from './components/ScrollToTop';
-import SimulationPage from './pages/SimulationPage';
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import ScrollToTop from "./components/ScrollToTop";
+import SimulationPage from "./pages/SimulationPage";
+import SupportPage from './pages/SupportPage';
 
 function App() {
   return (
@@ -14,9 +15,10 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='/login' element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/simulate" element={<SimulationPage />} />
+        <Route path="/support" element={<SupportPage />} />
       </Routes>
     </>
   );
